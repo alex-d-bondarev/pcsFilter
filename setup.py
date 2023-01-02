b578ff9229c76c393e5d49a9ef942110b4c22c15
Alex <5994478+alex-d-bondarev@users.noreply.github.com>
2023-01-02 11:39:12 +0000

    Bump up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="pcsFilter",
-    version="0.2.1",
+    version="0.2.2",
     python_requires=">=3.7",
     author="Sasha Bondarev (Oleksandr)",
     author_email="github.pcsfilter@gmail.com",
