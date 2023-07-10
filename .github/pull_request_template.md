1f80c59601c617906ce3d742a115b9f7614022a3
Sasha Bondarev (Oleksandr) <5994478+alex-d-bondarev@users.noreply.github.com>
2023-07-10 13:06:21 +0100

    i-26 Project CI (#89)
    * Init NOX.dockerfile
    
    * Docker experiments
    
    * Add nox github actions
    
    * Add more github action triggers
    
    * Remove action description
    
    * python-versions should be comma separated
    
    * Add install poetry step
    
    * Nox only on pull request
    
    * Add docker CI step
    
    * Fix docker job
    
    * Fix docker job run keyword
    
    * Try using make for docker step
    
    * Change test_all to e2e_test
    
    * Add / in docker file COPY step
    
    * Remove -it from the make command
    
    * Update test_pcsFilter_results to compare sets
    
    * Fix _assert_file_contents_are_the_same_in
    
    * Update CONTRIBUTING.md
    
    * Add publish-to-test-pypi.yaml
    
    * Update CONTRIBUTING.md
    
    * Publish only on new tag
    
    * Update CI flow names
    
    * Update PR template
    
    * Update numbered list style in CONTRIBUTING.md
    
    * Update headers styling in CONTRIBUTING.md

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -8,12 +8,11 @@ _PR description_
 - [ ] I have performed a self-review of this code
 - [ ] I have added tests (*)
 - [ ] PR is linked to an issue in the Development tab (**)
-- [ ] `make test_all` was executed before pushing the changes (***)
+- [ ] All PR checks have passed
 
 **Additional notes**
 - (*) You are not required to add new tests for implementation details 
   or documentation changes. 
 - (*) PR with a new/updated behaviour should include new or updated tests.
   Just tick the box if not applicable.
-- (**) Not applicable for security updates.
-- (***) Only required when code is added/updated. Just tick the box if not applicable.
+- (**) May be inapplicable for security updates.
--- tests/pcsFilter/e2e/test_pcsFilter_results.py
+++ tests/pcsFilter/e2e/test_pcsFilter_results.py
@@ -19,14 +19,14 @@ def test_pcsFilter_results():
 
 
 def _assert_files_are_the_same_in(dir_1, dir_2):
-    dir_1_contents = [path.name for path in dir_1.iterdir()]
-    dir_2_contents = [path.name for path in dir_2.iterdir()]
+    dir_1_contents = {path.name for path in dir_1.iterdir()}
+    dir_2_contents = {path.name for path in dir_2.iterdir()}
     assert dir_1_contents == dir_2_contents
 
 
 def _assert_file_contents_are_the_same_in(dir_1, dir_2):
-    dir_1_paths = [path for path in dir_1.iterdir()]
-    dir_2_paths = [path for path in dir_2.iterdir()]
+    dir_1_paths = sorted([path for path in dir_1.iterdir()])
+    dir_2_paths = sorted([path for path in dir_2.iterdir()])
 
     for file_1, file_2 in zip(dir_1_paths, dir_2_paths):
         assert ExistingFile(file_1).get_content() == \
